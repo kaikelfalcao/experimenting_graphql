c062c5ec9b40ba73e8627b0b15159a436b3769fd
Kaike Lima <kaike@sefaz.feiradesantana.ba.gov.br>
2024-01-02 16:20:34 -0300

    Implementing queries

--- community/lib/community_web/router.ex
+++ community/lib/community_web/router.ex
@@ -2,27 +2,16 @@ defmodule CommunityWeb.Router do
   use CommunityWeb, :router
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
-  scope "/api", CommunityWeb do
-    pipe_through :api
-  end
+  scope "/" do
+    pipe_through(:api)
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:community, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: CommunityWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
-    end
+    forward("/graphiql", Absinthe.Plug.GraphiQL,
+      schema: CommunityWeb.Schema,
+      interface: :simple,
+      context: %{pubsub: CommunityWeb.Endpoint}
+    )
   end
 end
